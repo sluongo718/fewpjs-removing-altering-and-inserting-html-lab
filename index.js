2ae0300f4547de036d216cf2f25ef177354bad00
sluongo718 <sluongo718@yahoo.com>
2021-06-19 00:46:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector('#main')
+main.remove()
+const body = document.querySelector('body')
+const newHeader =  document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "is the champion"
+body.appendChild(newheader)
